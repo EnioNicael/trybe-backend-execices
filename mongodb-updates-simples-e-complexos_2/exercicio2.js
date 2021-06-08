f1ccbaf15af8c9f97b51ead6f441c21ba568ecec
Enio Nicael <enionicael@gmail.com>
2021-06-08 18:58:01 -0300

    Solucao do exercicio 2.

---
+++ mongodb-updates-simples-e-complexos_2/exercicio2.js
@@ -0,0 +1,8 @@
+// Exercício 2: Utilizando o modificador $each , adicione as categorias "villain" e "comic-based" ao filme Batman .
+
+db.movies.updateOne(
+  { title: "Batman" },
+  { $push: { category: { $each: ["villain", "comic-based"]  }  } }
+);
+
+db.movies.findOne({ title: "Batman" });
